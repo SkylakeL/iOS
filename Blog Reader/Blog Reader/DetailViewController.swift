d5dadfffbe0d34802f3c5e4e0ddeaa19a5185533
SkylakeL <line2860@gmail.com>
2016-06-04 22:51:52 +0800

    built

---
+++ Blog Reader/Blog Reader/DetailViewController.swift
@@ -0,0 +1,49 @@
+//
+//  DetailViewController.swift
+//  Blog Reader
+//
+//  Created by YIN on 6/4/16.
+//  Copyright © 2016 Skylake. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    //@IBOutlet weak var detailDescriptionLabel: UILabel!
+    
+    @IBOutlet var webView: UIWebView!
+    
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let postWebView = self.webView {
+                
+                postWebView.loadHTMLString(detail.valueForKey("content")!.description, baseURL: NSURL(string: "https://googleblog.blogspot.com/"))
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
